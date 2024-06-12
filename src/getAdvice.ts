b0af8d75f5d581e165d6682a9c8a6d9ca171b2c4
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-06-12 13:30:53 +0200

    :sparkles: add axios.get

---
+++ src/getAdvice.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import type { IAdvice } from './models/Advice';
+
+export const getAdvice = async ():Promise<IAdvice> => {
+  const response = await axios.get<IAdvice>('https://api.adviceslip.com/advice')
+  return response.data
+
+  /*
+  /* Mocked api to not call every time
+      
+  
+  return {slip: {
+    "id": 128,
+    "advice": "When you're at a concert or event, enjoy the moment, enjoy being there. Try leaving your camera in your pocket."
+      }
+    }
+   */
+    /* API - call add async to function
+  
+
+    */
+}
