fcee94d037284e8b559ec87eacb33c6078a5814c
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-06-12 13:32:48 +0200

    :sparkles: render advice§

---
+++ src/components/AdviceRender.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import type { IAdvice } from '../models/Advice'
+
+interface IAdviceProps {
+    advice: IAdvice
+}
+ const props = defineProps<IAdviceProps>();
+
+
+</script>
+
+<template>
+<div class="advice">
+    <p>Advice {{ advice.slip.id}}</p>
+    <h2>{{ advice.slip.advice }}</h2>
+</div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/RandomAdviceBtn.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+defineEmits<{
+    (e: 'randomAdvice'):void
+}>()
+</script>
+
+<template>
+
+    <div>
+        <img @click="$emit('randomAdvice')" src="../../public/images/icon-dice.svg" alt="Dice to randomize an advice">
+    </div>
+
+
+</template>
+
+<style scoped>
+</style>
